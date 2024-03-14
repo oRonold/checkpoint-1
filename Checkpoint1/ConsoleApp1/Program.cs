97333a9a1f4c1d9cd65f32a287dac896d6266b83
oRonold <or040918@gmail.com>
2024-03-14 19:52:23 -0300

    Finalizando cp

--- Checkpoint1/ConsoleApp1/Program.cs
+++ Checkpoint1/ConsoleApp1/Program.cs
@@ -1,57 +1,189 @@
-﻿Aluno al = new Aluno("Ronald", 19, "ADS");
+﻿/*
+    CHECKPOINT 1 - Advanced Business Development with .NET
+    Integrante:
+    Ronald de Oliveira Farias - RM 552364
 
-al.Apresentar();
-
-//Classe abstrata nao pode ser instanciada
+    Tema: Sistema de Gestao Escolar
+ */
 public abstract class Pessoa
 {
-    //Variaveis
-    private string _nome;
 
-    //Propriedades
     public string Nome { get; }
-    public int Idade { get; set; }
+    public int Idade { get; }
 
-    //Construtores
     public Pessoa()
     {
 
     }
 
-    //Definindo que o atributo _nome recebera o valor como o parametro nome do construtor
-    //A Propriedade Nome recebera o valor de _nome
     public Pessoa(string nome, int idade)
     {
-        this._nome = nome;
-        this.Nome = _nome;
+        this.Nome = nome;
         this.Idade = idade;
     }
 
-    //Metodos
-    //virtual significa que o metodo pode ser sobreescrito por outras classes filhas 
     public virtual void Apresentar()
     {
-        Console.WriteLine($"Meu nome é {this.Nome} e minha idade é {this.Idade} anos");
+        Console.WriteLine($"Me chamo {this.Nome}, tenho {this.Idade} anos");
+    }
+}
+
+public class Sistema
+{
+    private List<Aluno> ListaDeAlunos = new List<Aluno>();
+    private List<Professor> ListaDeProfessores = new List<Professor>();
+
+    public void CadastrarAluno(int idade)
+    {
+        Console.WriteLine("Digite o nome do aluno: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Digite o periodo do aluno: ");
+        string periodo = Console.ReadLine();
+        Console.WriteLine("Digite a matricula do aluno: ");
+        string matricula = Console.ReadLine();
+
+        Aluno aluno = new Aluno(nome, idade, periodo, matricula);
+        ListaDeAlunos.Add(aluno);
+        Console.WriteLine("Aluno cadastrado com sucesso!");
+    }
+
+    public void exibeListaAlunos()
+    {
+        foreach(Aluno aluno in ListaDeAlunos)
+        {
+            Console.WriteLine("\nAluno " + aluno.Nome + "\nIdade " + aluno.Idade + "\nPeriodo " + aluno.Periodo + "\nMatricula: ");
+        }
+    }
+
+    public void ExibeListaProfessores()
+    {
+        foreach(Professor professor in ListaDeProfessores)
+        {
+            Console.WriteLine("\nProfessor " + professor.Nome + "\nIdade " + professor.Idade + "\nGraduacao " + professor.Graduacao + "\nDisciplina: " + professor.Disciplina);
+            professor.Apresentar();
+        }
+    }
+
+    public void CadastrarProfessor(int idade)
+    {
+        Console.WriteLine("Digite o nome do professor: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Digite a graduação do professor: ");
+        string graduacao = Console.ReadLine();
+        Console.WriteLine("Digite a disciplina do professor: ");
+        string disciplina = Console.ReadLine();
+
+        Professor professor = new Professor(nome, idade, graduacao, disciplina);
+        ListaDeProfessores.Add(professor);
+        Console.WriteLine("Professor cadastrado com sucesso!");
     }
 
 }
 
-//Fazemos herança com :
-public class Aluno : Pessoa
+internal class Aluno : Pessoa
 {
-    public string Curso { get; set; }
+    public string Periodo { get; }
+    protected string NumeroMatricula { get; }
 
-    //Criando construtor sendo classe filha
-    public Aluno(string nome, int idade, string curso) : base(nome, idade)
+    public Aluno(string nome, int idade, string periodo, string matricula) : base(nome, idade)
     {
-        this.Curso = curso;
+        this.Periodo = periodo;
+        this.NumeroMatricula = matricula;
+    } 
+}   
+
+public class Professor : Pessoa
+{
+    public string Disciplina { get; }
+    public string Graduacao { get; }
+
+    public Professor(string nome, int idade, string graduacao, string disciplina) : base(nome, idade)
+    {
+        this.Disciplina = disciplina;
+        this.Graduacao = graduacao;
     }
 
-    //Sobreescrevendo metodo Apresentar da classe Pessoa
     public override void Apresentar()
     {
-        base.Apresentar();
-        Console.WriteLine($"e o meu curso é {this.Curso}");
+        Console.WriteLine($"Me chamo {this.Nome}, tenho {this.Idade} anos, sou formado em {this.Graduacao} e leciono a disciplina de {this.Disciplina}");
     }
 
-}
+}   
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sistema sistema = new Sistema();
+
+        int opcao = 0;
+
+        while (true)
+        {
+            Console.WriteLine("\nBem-vindo ao sistema de gestão escolar");
+            Console.WriteLine("1 - Cadastrar Aluno");
+            Console.WriteLine("2 - Cadastrar Professor");
+            Console.WriteLine("3 - Exibir lista de alunos");
+            Console.WriteLine("4 - Exibir lista de professores");
+            Console.WriteLine("5 - Sair");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            if(opcao == 1)
+            {
+                Console.WriteLine("Quantos alunos voce deseja cadastrar?");
+                int qtd = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a idade do aluno mais novo: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < qtd; i++)
+                {
+                    if(idade >= 6)
+                    {
+                        sistema.CadastrarAluno(idade);
+                    } else
+                    {
+                        Console.WriteLine($"O aluno precisa ter no mínimo 6 anos para cursar o ensino fundamental");
+                        continue;
+                    }
+                    
+                }   
+            }
+            if(opcao == 2)
+            {
+                Console.WriteLine("Qual a idade do professor?");
+                int idade = int.Parse(Console.ReadLine());
+                if(idade < 18)
+                {
+                    Console.WriteLine("O professor precisa ter no mínimo 18 anos para lecionar");
+                    continue;
+                } else
+                {
+                    sistema.CadastrarProfessor(idade);
+                    continue;
+                }
+            }
+          if(opcao == 3)
+            {
+                Console.WriteLine("Lista de alunos cadastrados: ");
+                sistema.exibeListaAlunos();
+                continue;
+            }
+          if(opcao == 4)
+            {
+                Console.WriteLine("Lista de professores cadastrados: ");
+                sistema.ExibeListaProfessores();
+                continue;
+            }
+          if(opcao == 5)
+            {
+                Console.WriteLine("Saindo do sistema...");
+                break;
+            }
+
+        }
+
+        
+    }
+}   
